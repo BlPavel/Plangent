2231154cc4d87b27cc89f3faf7cf3afdf7a59903
Pavel <pavel@MacBook-Air-Pavel.local>
2026-07-22 18:50:48 +0400

    fix dev run

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
     },
   },
   server: {
+    host: '127.0.0.1',
     port: 5173,
     proxy: {
       '/api': { target: 'http://localhost:3000', changeOrigin: true },
